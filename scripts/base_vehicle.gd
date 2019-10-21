10f2ce6857c18c4cc50c8a4254f588c430f837cf
mikahimself <mika.laihanen@gmail.com>
2019-10-21 07:41:07 +0300

    Add initial car handling based on RigidBody2D

--- scripts/base_vehicle.gd
+++ scripts/base_vehicle.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -6,8 +6,14 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 
-export (int) var speed = 200
-export (float) var rotation_speed = 1.5
+export (int) var acceleration = 100
+export (float) var speed = 0
+export (int) var engine_power = 200
+export (float) var rotation_speed = 1350
+
+# Use Rigidbody's angular damp to slow down rotation.
+
+#export (float) var friction = 0.1
 
 func _ready():
 	pass # Replace with function body.
@@ -17,17 +23,43 @@ var rotation_dir = 0
 	
 func get_input():
 	rotation_dir = 0
+	speed = 0
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
-		rotation_dir += 1
+		rotation_dir = 1
 	if Input.is_action_pressed('left'):
-		rotation_dir -= 1
+		rotation_dir = -1
 	if Input.is_action_pressed('down'):
-		velocity = Vector2(-speed, 0).rotated(rotation)
+		speed = -1
+		#velocity = Vector2(-speed, 0).rotated(rotation)
 	if Input.is_action_pressed('up'):
-		velocity = Vector2(speed, 0).rotated(rotation)
+		speed = 1
+	
+	#velocity = Vector2(speed * enginepower, 0).rotated(rotation)
 	
 func _physics_process(delta):
+	
+
 	get_input()
-	rotation += rotation_dir * rotation_speed * delta
-	velocity = move_and_slide(velocity)
+	
+	# nopeus
+	#if (speed == 80):
+	#	apply_central_impulse(Vector2(5, 0).rotated(rotation))
+	
+		
+	# Transforming stuffs from Unity. Transform.Up is the direction an object is pointing to in Unity
+	# In Godot, it is Vector2.RIGHT rotated towards rotation.
+	var forwards_velocity = Vector2.RIGHT.rotated(rotation) * linear_velocity.dot(Vector2.RIGHT.rotated(rotation))
+	var sideways_velocity = Vector2.UP.rotated(rotation) * linear_velocity.dot(Vector2.UP.rotated(rotation))
+	#print(liirtospiidi)
+	
+	#rotation += rotation_dir * rotation_speed * delta
+	#velocity = move_and_slide(velocity)
+	#set_applied_force()
+
+	#print(Vector2(speed, 0).rotated(rotation))
+
+	set_applied_force(Vector2(speed * engine_power, 0).rotated(rotation))
+	set_linear_velocity(forwards_velocity + sideways_velocity * 0.95)
+	
+	set_applied_torque(rotation_dir * rotation_speed)
